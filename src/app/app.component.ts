85b53d9c349218f1d020505001a4409ec7e25f14
jasonfritsche <fritsche.jason@gmail.com>
2023-10-08 18:04:40 -0500

    feat: create map component using leaflet lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './features/map/map.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent],
 })
 export class AppComponent {
   title = 'air-quality-now';
